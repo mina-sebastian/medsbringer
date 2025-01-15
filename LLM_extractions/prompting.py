bf526e1f622146ce5efe7d00de20b68bba992abf
Alexandra06T <email.alexandra.toma@gmail.com>
2025-01-15 23:15:07 +0200

    first chat message

---
+++ LLM_extractions/prompting.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+    ]
+)
+
+print(completion.choices[0].message)
