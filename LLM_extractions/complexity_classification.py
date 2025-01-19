b8f974b3183df8ba84444d69d4b7cdc54162a8ce
Alexandra06T <email.alexandra.toma@gmail.com>
2025-01-19 17:06:59 +0200

    added classification with pos translation of sentences

--- LLM_extractions/complexity_classification.py
+++ LLM_extractions/complexity_classification.py
@@ -7,7 +7,7 @@ import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.pipeline import make_pipeline
 
 def prepare_data_with_label_1(filenames):
@@ -49,6 +49,9 @@ def prepare_data_with_label_0(filenames, sentences_1, sentences_1_mean_len, sent
         sentences.extend(file_sentences)
 
     sentences = np.unique(sentences)
+
+    # take only some of the sentences labeled 0, so as to be proportional to the sentences labeled 1
+    sentences = np.random.choice(sentences, sentences_1.shape[0])
     labels = [0 for _ in range(sentences.shape[0])]
 
     return sentences, labels
@@ -56,8 +59,16 @@ def prepare_data_with_label_0(filenames, sentences_1, sentences_1_mean_len, sent
 def prepare_data(filenames):
     sentences_1, labels_1, sentences_1_mean_len, sentences_1_std_len = prepare_data_with_label_1(filenames)
     sentences_0, labels_0 = prepare_data_with_label_0(filenames, sentences_1, sentences_1_mean_len, sentences_1_std_len)
-    print('0 label: ', sentences_0.shape)
-    print('1 label: ', sentences_1.shape)
+    
+    with open('LLM_extractions/results/sentence_complexity_classification_results_stop_words.txt', 'w') as file:
+        file.write('Sentences labeled 0: ' + str(sentences_0.shape[0]) + '\n')
+        file.write('Sentences labeled 1: ' + str(sentences_1.shape[0]) + '\n')
+    with open('LLM_extractions/results/sentence_complexity_classification_results_pos.txt', 'w') as file:
+        file.write('Sentences labeled 0: ' + str(sentences_0.shape[0]) + '\n')
+        file.write('Sentences labeled 1: ' + str(sentences_1.shape[0]) + '\n')
+    print('Sentences labeled 0: ', sentences_0.shape[0])
+    print('Sentences labeled 1: ', sentences_1.shape[0])
+    
     data = np.concatenate((sentences_1, sentences_0), axis=0)
     labels = labels_1 + labels_0
 
@@ -66,33 +77,45 @@ def prepare_data(filenames):
 
     return data, labels
 
-# def translate_to_pos(data):
+
+def translate_to_pos(data):
+    pos_data = []
+    for sentence in data:
+        tokens = nlp(str(sentence))
+        translated_sentence = ' '.join([token.pos_ for token in tokens])
+        pos_data.append(translated_sentence)
+
+    return np.array(pos_data)
 
 
-def train_tf_idf_vectorizer(vocabulary, data, labels):
-    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.3, random_state = 0)
+def train_tf_idf_vectorizer(data, labels, results_filename, vocabulary=None):
+    with open(results_filename, 'a') as file:
+        
+        X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.2, random_state = 0)
 
-    print('train:', X_train.shape)
-    print('test:', X_test.shape)
+        file.write('\nTrain data: ' + str(X_train.shape[0]))
+        file.write('\nTest data: ' + str(X_test.shape[0]) + '\n')
+        print('train:', X_train.shape)
+        print('test:', X_test.shape)
 
-    model_tfidf = make_pipeline(TfidfVectorizer(vocabulary=vocabulary), MultinomialNB())
-    model_tfidf.fit(X_train, y_train)
-    y_pred_tfidf = model_tfidf.predict(X_test)
+        if vocabulary is None:
+            model_tfidf = make_pipeline(TfidfVectorizer(), MultinomialNB())
+        else:
+            model_tfidf = make_pipeline(TfidfVectorizer(vocabulary=vocabulary), MultinomialNB())
 
-    f1 = f1_score(y_test, y_pred_tfidf, average='weighted')
-    accuracy = accuracy_score(y_test, y_pred_tfidf)
+        model_tfidf.fit(X_train, y_train)
+        y_pred_tfidf = model_tfidf.predict(X_test)
 
-    print('Multinomial Naive Bayes with TF-IDF:')
-    print('-' * 40)
-    print(f'f1: {f1:.4f}')
-    print(f'accuracy: {accuracy:.4f}')
+        report = classification_report(y_test, y_pred_tfidf)
 
-    report = classification_report(y_test, y_pred_tfidf)
-
-    print('\n Accuracy: ', accuracy_score(y_test, y_pred_tfidf))
-    print('\nClassification Report')
-    print('======================================================')
-    print('\n', report)
+        file.writelines(['\nAccuracy: ', str(accuracy_score(y_test, y_pred_tfidf))])
+        file.write('\n\nClassification Report\n')
+        file.write('======================================================')
+        file.writelines(['\n', report])
+        print('\n Accuracy: ', accuracy_score(y_test, y_pred_tfidf))
+        print('\nClassification Report')
+        print('======================================================')
+        print('\n', report)
 
 
 
@@ -100,9 +123,14 @@ def train_tf_idf_vectorizer(vocabulary, data, labels):
 llm_folder = "LLM_extractions/results/llm_results"
 filenames = [f for f in os.listdir(llm_folder) if f.endswith(".json")]
 filenames.sort(key=lambda x : int(x.split('_')[0]))
-# filenames = ["0_excerpts.json", "1_excerpts.json", "2_excerpts.json", "3_excerpts.json", "4_excerpts.json"]
 
 nlp = spacy.load("ro_core_news_sm")
 data, labels = prepare_data(filenames)
-print(data.shape)
-train_tf_idf_vectorizer(nlp.Defaults.stop_words, data, labels)
+print('Data shape', data.shape)
+
+# classify using stop words
+train_tf_idf_vectorizer(data, labels, 'LLM_extractions/results/sentence_complexity_classification_results_stop_words.txt', nlp.Defaults.stop_words)
+
+# classify using pos
+translated_data = translate_to_pos(data)
+train_tf_idf_vectorizer(translated_data, labels, 'LLM_extractions/results/sentence_complexity_classification_results_pos.txt')
